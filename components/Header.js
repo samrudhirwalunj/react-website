dad7572b76a05f1afbdcea8e40703014ab6d1ede
samrudhirwalunj <samrudhirwalunj@gmail.com>
2026-04-17 11:32:45 +0530

    Header Footer Changes

---
+++ components/Header.js
@@ -0,0 +1,59 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import '../styles/header.scss';
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="flex items-center space-x-1 group cursor-pointer">
+            <img
+              src="/logo.png"
+              alt="Jenlor"
+              className={` logo-img transition-all duration-300 ${
+                scrolled
+                  ? "large"
+                  : "small"
+              }`}
+            />
+          </div>
+
+        {/* Desktop Nav */}
+        <nav className="nav desktop">
+          <Link href="/">Home</Link>
+          <Link href="/tabs">Tabs</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
+
+        {/* Hamburger */}
+        <div
+          className={`hamburger ${isOpen ? 'active' : ''}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      <nav className={`nav mobile ${isOpen ? 'open' : ''}`}>
+        <Link href="/" onClick={() => setIsOpen(false)}>Home</Link>
+        <Link href="/login" onClick={() => setIsOpen(false)}>Login</Link>
+        <Link href="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+      </nav>
+    </header>
+  );
+}
